249553bef3b222a6f4abba71afb29371df2e07b7
Rafael Nunes <nunesrafael663@gmail.com>
2022-03-21 23:58:41 -0300

    Iniciando o projeto com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo" >Home</h1>
+        <img src="https://i.pinimg.com/originals/48/f3/0a/48f30abbdea5e1fc50d1b1b1b521c1ad.jpg" 
+        alt="Imagem da Tela Inicial" className="img" />
+        </>
+    ); 
+}
+
+export default Home;
