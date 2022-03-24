3247e355f1593521ecdb698baf801a49125db116
Rafael Nunes <nunesrafael663@gmail.com>
2022-03-23 23:56:49 -0300

    Adicionando Navbar, Footer e componentes flexbox

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import {Typography, Box, Grid } from '@material-ui/core';
+
+function Footer() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#1F1F25", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                             <a href="https://www.linkedin.com/in/rrafaelnunes/" target="_blank">
+                                <LinkedInIcon style={{ fontSize: 65, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/RafaelNunesDaSilva" target="_blank">
+                                <GitHubIcon style={{ fontSize:48, color: "white" }} />
+                            </a>
+                            <a href="https://www.instagram.com/ragael_nunes/" target="_blank">
+                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                           
+                        </Box>
+                    </Box>
+                    <Box style={{ backgroundColor: "#19191E", height: "60px" }}>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                            </a>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Footer;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
-function Home(){
+function Home() {
     return (
         <>
-        <h1 className="titulo" >Home</h1>
-        <img src="https://i.pinimg.com/originals/48/f3/0a/48f30abbdea5e1fc50d1b1b1b521c1ad.jpg" 
-        alt="Imagem da Tela Inicial" className="img" />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#26262F" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "#EFC13B", fontWeight: "bold" }}>Seja bem vindo(a), ao Urbanic Blog!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#EFC13B", fontWeight: "bold" }}>Aqui compartilhamos nossos rolês e experiências, aos arredores da maravilhosa cidade de São Paulo. </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "#EFC13B", backgroundColor: "#26262F", color: "#EFC13B" }}>Compartilhe!</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/SoB8B8T.png" alt="Arte feita em um prédio na grande São Paulo" width="500px" height="527px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "#26262F" }}>
+                </Grid>
+            </Grid>
         </>
-    ); 
+    );
 }
 
 export default Home;
