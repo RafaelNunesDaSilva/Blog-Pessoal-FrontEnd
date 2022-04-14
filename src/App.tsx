0b8c56ed8d982032a65cb76aa72d5fcfea4e3756
Rafael Nunes <nunesrafael663@gmail.com>
2022-04-14 11:57:34 -0300

    Ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import CadastroUsuario from './paginas/CadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
